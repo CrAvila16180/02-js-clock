d36d843e9e2be94d2c996c1a8019c2cdaeaa9581
CR_Avila <carlosrobertoavilah@gmail.com>
2018-12-23 01:22:50 -0600

    Make hands move

---
+++ main.js
@@ -0,0 +1,50 @@
+const log = (content) => console.log(content);
+
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+let hourDisplay = document.querySelector('#hourDisplay');
+let minuteDisplay = document.querySelector('#minuteDisplay');
+let secondDisplay = document.querySelector('#secondDisplay');
+
+let textHour = document.querySelector('.textHour');
+
+let moveSeconds = (now) => {
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds/60)*360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+   
+    secondDisplay.textContent = `${seconds}`
+}
+
+let moveMinutes = (now) => {
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes/60)*360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    log(minutes);
+    minuteDisplay.textContent = `${minutes}:`;
+    
+}
+
+let moveHours = (now) => {
+    const hours = now.getHours();
+    const hoursDegrees = ((hours/12)*360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    log(hours);
+    hourDisplay.textContent = `${hours}:`;
+    
+}
+
+let setDate = () => {
+
+    const now = new Date();
+    moveSeconds(now);
+    moveMinutes(now);
+    moveHours(now);
+
+    textHour.textContent = `${hours}:${minutes}:${seconds}`;
+    
+};
+
+setInterval(setDate, 1000);
